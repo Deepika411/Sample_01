2a68e12a408bb14e42adf470d1776d2cbd97f435
DevikaTesting <k.devi.divi@gmail.com>
2019-03-21 06:32:20 +0530

    add method created

---
+++ src/org/test/Addition.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Addition {
+public void Employee() {
+	System.out.println("Dinesh");
+}
+public static void main(String[] args) {
+	System.out.println("method created");
+}
+}
