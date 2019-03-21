35f3b8822dd5f6a39cd04ff9f0d8ef1bed390c5d
DevikaTesting <k.devi.divi@gmail.com>
2019-03-21 06:35:44 +0530

    sub created

---
+++ src/org/test/Sub.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Sub {
+private void Subtraction() {
+    System.out.println("sub method");	
+}
+public static void main(String[] args) {
+	System.out.println("method created");
+}
+}
